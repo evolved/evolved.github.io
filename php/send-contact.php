64379607f045801150cafdc5141347f35a527aa1
Dennis <dennis@evolved.io>
2014-09-27 23:05:08 -0700

    Statische Seite hinzugefügt

---
+++ php/send-contact.php
@@ -0,0 +1,7 @@
+<?php
+
+// Please put your PHP code here
+//
+// You can use entered name variable as $_POST['contact_name'], e-mail variable as $_POST['contact_email'], message variable as $_POST['contact_message']
+
+?>
---
+++ php/send-subscription.php
@@ -0,0 +1,7 @@
+<?php
+
+// Please put your PHP code here
+//
+// You can use entered e-mail variable as $_POST['subscription']
+
+?>
