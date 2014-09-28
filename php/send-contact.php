91f7f1ab3f5fc823bb9b8dbc4651d667117264eb
Dennis <dennis@evolved.io>
2014-09-27 23:07:27 -0700

    php und twitter ordner entfernt

--- php/send-contact.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Please put your PHP code here
-//
-// You can use entered name variable as $_POST['contact_name'], e-mail variable as $_POST['contact_email'], message variable as $_POST['contact_message']
-
-?>
--- php/send-subscription.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Please put your PHP code here
-//
-// You can use entered e-mail variable as $_POST['subscription']
-
-?>
